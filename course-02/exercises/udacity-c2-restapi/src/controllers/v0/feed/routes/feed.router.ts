d75fde667006b9917324a309c5dc0cf8b19f6bad
Mike Papesca <michael.papesca@trove.co>
2022-07-21 14:21:33 -0600

    Add auth requirements

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -54,7 +54,6 @@ router.patch('/:id', requireAuth, async (req: Request, res: Response) => {
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName', requireAuth, async (req: Request, res: Response) => {
     let { fileName } = req.params;
-    console.log({ fileName });
     const url = AWS.getPutSignedUrl(fileName);
     res.status(201).send({ url: url });
 });
